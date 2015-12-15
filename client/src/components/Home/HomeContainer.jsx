c5e054c8c5be6a519a908c5c4b698e7e939251fc
Laura Knight <ljknight@gmail.com>
2015-12-14 16:03:58 -0800

    Update pagination

--- client/src/components/Home/HomeContainer.jsx
+++ client/src/components/Home/HomeContainer.jsx
@@ -6,7 +6,6 @@ var Spinner = require('react-spin');
 
 var Title = require('./../Title.jsx');
 var IssueFeed = require('./IssueFeed.jsx');
-var Pagination = require('./Pagination.jsx');
 var Constants = require('./../../Constants.js');
 var APIkey = require('./../../APIkey.js');
 
@@ -17,21 +16,13 @@ var HomeContainer = React.createClass({
       issues: [],
       prevIssues: [],
       data: [],
-      page: this.props.params.page || 1
+      page: this.props.params.page || 1,
+      lastPage: ''
     };
   },
 
   componentDidMount: function() {
-    var currPage = this.state.page;
-    var prevPage = this.state.page - 1;
-    
-    if (currPage > 1) {
-      // Gets prev page issues & calls getIssues to handle async responses
-      this.getPrevIssues(prevPage);
-    } else {
-      this.getIssues();
-    }
-
+    this.paginate(this.state.page);
   },
 
   // Scroll to top after page change
@@ -39,7 +30,48 @@ var HomeContainer = React.createClass({
     ReactDOM.findDOMNode(document.body).scrollTop = 0;
   },
 
-  getPrevIssues: function(APIpage) {
+  paginate:function(page) {
+    var nextPage = Number(this.state.page) + 1;
+    var prevPage = Number(this.state.page) - 1;
+    var prevAPIPage;
+    var APIpage;
+
+    // Step tells us how many API pages back we need to look
+    var step = Math.ceil(page/6);
+
+    if (page === 1) {
+      this.getIssues(page);
+      return;
+    } else if (page === 'next') {
+      prevAPIPage = nextPage - step;
+      APIpage = nextPage - step + 1;
+      // Set new current page
+      this.setState({
+        page: nextPage
+      });
+
+    } else if (page === 'prev') {
+      prevAPIPage = prevPage - step;
+      APIpage = prevPage - step + 1;
+      // Set new current page
+      this.setState({
+        page: prevPage
+      });
+
+    } else {        
+      prevAPIPage = page - step;
+      APIpage = page - step + 1;
+      // Set new current page
+      this.setState({
+        page: page
+      });
+    }
+    
+    // Get issues needed for page 
+    this.getPrevIssues(prevAPIPage, APIpage);
+  },
+
+  getPrevIssues: function(prevAPIPage, APIpage) {
     // Get previous page's issues in background for pagination
     $.ajax({
       url: 'https://api.github.com/repos/npm/npm/issues?page=' + APIpage + '&' + APIkey,
@@ -50,7 +82,8 @@ var HomeContainer = React.createClass({
             prevIssues: data
           });
         }
-        this.getIssues();
+        // Chained call to catch both async responses
+        this.getIssues(APIpage);
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -66,9 +99,13 @@ var HomeContainer = React.createClass({
       url: 'https://api.github.com/repos/npm/npm/issues?page=' + APIpage + '&' + APIkey,
       dataType: 'json',
       success: function(data, status, request) {
+        // var header = request.getResponseHeader('Link');
+        // var lastPage = this.parseHeaders(header).last;
+
         if (this.isMounted()) {
           this.setState({
-            data: data
+            data: data,
+            // lastPage: this.findLastPage(lastPage)
           });
           this.showIssues(data);
         }
@@ -79,25 +116,35 @@ var HomeContainer = React.createClass({
     });
   },
 
-  paginate:function(page) {
-    var nextPage = Number(this.state.page) + 1;
-    var prevPage = Number(this.state.page) - 1;
+  // Credit: https://gist.github.com/niallo/3109252
+  // parseHeaders: function(header) {
+  //   if (header.length === 0) {
+  //     throw new Error("input must not be of zero length");
+  //   }
 
-    if (page === 'next') {
-      this.getIssues(nextPage);
-      // Set new current page
-      this.setState({
-        page: nextPage
-      });
-    } else if (page === 'prev') {
-      this.getIssues(prevPage);
-      // Set new current page
-      this.setState({
-        page: prevPage
-      });
-    }
+  //   // Split parts by comma
+  //   var parts = header.split(',');
+  //   var links = {};
+    
+  //   // Parse each part into a named link
+  //   parts.forEach(function(p) {
+  //     var section = p.split(';');
+  //     if (section.length != 2) {
+  //       throw new Error("section could not be split on ';'");
+  //     }
+  //     var url = section[0].replace(/<(.*)>/, '$1').trim();
+  //     var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+  //     links[name] = url;
+  //   });
 
-  },
+  //   return links;
+  
+  // },
+
+  // findLastPage: function(lastLink) {
+  //   var matches = lastLink.match(/page=([^&]*)/);
+  //   return matches[1];
+  // },
 
   // Load 25 per page
   showIssues: function(currAPIPageIssues) {  
@@ -110,11 +157,11 @@ var HomeContainer = React.createClass({
       }
     } else {
       // Get previous API page's issues for displaying
-      var prevPage = page-1;
       var prevAPIPageIssues = this.state.prevIssues;
+      console.log(prevAPIPageIssues, currAPIPageIssues)
 
-      // Pattern for pagination
-      var dictionary = {
+      // Patterns for pagination
+      var issuesPerPage = {
         1: {
           prevPage: [0,24]
         },
@@ -134,12 +181,14 @@ var HomeContainer = React.createClass({
           prevPage: [10,29],
           page: [0,4]
         },
-        6: {
+        0: {
           prevPage: [5,29]
         }
       };
 
-      var lookup = dictionary[page % 6];
+      // Lookup tells us which items on those pages we need
+      var lookup = issuesPerPage[page % 6];
+      console.log(page, page % 6)
       
       var get25 = function(obj) {
         var prevPageArr = obj.prevPage;
@@ -150,7 +199,7 @@ var HomeContainer = React.createClass({
           currentIssues.push(prevAPIPageIssues[i]);
         }
 
-        // Add any issues from current API page
+        // Add issues from current API page
         if (pageArr) {
           for (var j = pageArr[0]; j <= pageArr[1]; j++) {
             currentIssues.push(currAPIPageIssues[j]);
@@ -159,8 +208,6 @@ var HomeContainer = React.createClass({
       };
 
       get25(lookup);
-      console.log(currentIssues)
-
     }
 
     // Set new issues to render page
@@ -173,6 +220,14 @@ var HomeContainer = React.createClass({
     var $loading = $('.spinner');
     var currPage = Number(this.state.page);
 
+    // Hide prev link on first page & next on last page
+    if (currPage === 1) {
+      $('.prev').hide();
+    } else if (currPage === this.state.lastPage) {
+      $('.next').hide();
+    }
+
+    // Spinner
     $(document)
       .ajaxStart(function () {
         $loading.show();
@@ -191,16 +246,14 @@ var HomeContainer = React.createClass({
           <IssueFeed issues={this.state.issues} />
           <nav className='pagination-nav'>
             <ul className='pagination'>
-              <li onClick={this.paginate.bind(this,'prev')}><Link to={`/page/${currPage - 1}`}> Previous</Link></li>
-              <li onClick={this.paginate.bind(this, 'next')}><Link to={`/page/${currPage + 1}`}> Next</Link></li>
+              <li className='prev' onClick={this.paginate.bind(this,'prev')}><Link to={`/page/${currPage - 1}`}> Previous</Link></li>
+              <li className='next' onClick={this.paginate.bind(this, 'next')}><Link to={`/page/${currPage + 1}`}> Next</Link></li>
             </ul>
           </nav>
         </div>
       </div>
     )
   }
-          // <Pagination getIssues={this.getIssues} page={this.state.page} data={this.state.data} />
-
 });
 
 module.exports = HomeContainer;
